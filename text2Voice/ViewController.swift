bb5b0a9dda731da4e1b16c14b06361533d9894f8
Huang Grace <huang.yahui.grace@gmail.com>
2020-01-04 15:51:27 +0800

    Initial Commit

---
+++ text2Voice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  text2Voice
+//
+//  Created by grace on 2020/1/4.
+//  Copyright © 2020 grace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
